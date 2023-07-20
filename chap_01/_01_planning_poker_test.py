eaa51ef61b6d70ebd56f580d1a55ca0ea40cc882
Rafael Capaci <rafaelcapacipereira@gmail.com>
2023-07-20 11:30:35 -0300

    Chap 1: Add first property based test using hypothesis

--- chap_01/_01_planning_poker_test.py
+++ chap_01/_01_planning_poker_test.py
@@ -1,3 +1,6 @@
+from random import shuffle
+
+from hypothesis import given, strategies as st
 import pytest
 
 from ._01_planning_poker import identify_extremes, Estimate
@@ -33,3 +36,37 @@ def test_reject_empty_list():
 def test_reject_single_estimate():
     with pytest.raises(ValueError, match='There has to be more than 1 estimate in the list'):
         identify_extremes([Estimate('Eleanor', 1)])
+
+
+@given(
+    st.lists(
+        st.tuples(
+            st.text(min_size=1, max_size=2),
+            st.integers(2, 99)
+        ),
+        min_size=2,
+        max_size=5,
+    ))
+def test_many_estimates(estimates):
+    """
+    Hypothesis will generate a random list of estimates.
+    
+    With the parameters, we ensure the estimates will always be in the range 2..99
+    Then we insert 1 and 100 to ensure the min and max estimates are known.
+    """
+    estimates_input = [
+        Estimate(estimate[0], estimate[1])
+        for estimate in 
+    estimates]
+
+    estimates_input.append(Estimate('MinDev', 1))
+    estimates_input.append(Estimate('MaxDev', 100))
+
+    shuffle(estimates_input)
+
+    result = identify_extremes(estimates_input)
+
+
+    assert len(result) == 2
+    assert 'MinDev' in result
+    assert 'MaxDev' in result
